0af2e701f61a3b77ebd8d14506ef916e1c56b582
Paul Le Cam <paul@3box.io>
2020-10-12 16:11:12 +0100

    Add 3ID keychain constants

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { IDXPublishedDefinitions, IDXPublishedSchemas } from '@ceramicstudio/idx
 
 export const definitions: IDXPublishedDefinitions = {
   basicProfile: 'ceramic://bagcqcerakszw2vsovxznyp5gfnpdj4cqm2xiv76yd24wkjewhhykovorwo6a',
-  cryptoAccountLinks: 'ceramic://bagcqcerab3lf5regasdioanst6buv7qlbxtj3kfnq2uzajaaynxsxjdeoedq'
+  cryptoAccountLinks: 'ceramic://bagcqcerab3lf5regasdioanst6buv7qlbxtj3kfnq2uzajaaynxsxjdeoedq',
+  threeIdKeychain: 'ceramic://bagcqceralomo65ag34ymh37ciaclr2qrum37b74jwyzawf23r2iypuufpjya'
 }
 
 export const schemas: IDXPublishedSchemas = {
   BasicProfile: 'ceramic://bagcqcerax2dnnittgudhusua5abfvebkbps2pt5ppyfetyuqnqcusogr3wsa',
   CryptoAccountLinks: 'ceramic://bagcqcerapvke7vlokqdemay2rfd36rdg6azhqe37xum76nwjmam2bnozssqa',
   Definition: 'ceramic://bagcqcerayzl65zpukldtsp2bgy4rhsvdagkba27osgzepnw3xg5bhzg4dcwq',
-  IdentityIndex: 'ceramic://bagcqceragcfkpjmv722fdse3sfhvx2jcezlb2avvg63fmxbkjoczhsq3rlfa'
+  IdentityIndex: 'ceramic://bagcqceragcfkpjmv722fdse3sfhvx2jcezlb2avvg63fmxbkjoczhsq3rlfa',
+  ThreeIdKeychain: 'ceramic://bagcqcerawqxf7b6s2obd2gpdzzu3yl47metomi4oxm7kszrojbuxfuv5grwa'
 }
