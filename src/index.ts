e736ff7d9d0201aaae8eebe08f304d3c847de758
Paul Le Cam <paul@3box.io>
2021-01-19 13:08:33 +0000

    Update definition schema

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,6 @@ export type Definition<C extends Record<string, any> = Record<string, any>> = {
   description: string
   schema: string
   url?: string
-  family?: string
   config?: C
 }
 
@@ -82,7 +81,7 @@ export type PublishedSchemas = PublishedRecord<SchemaName>
 export const schemas: PublishedSchemas = {
   BasicProfile: 'ceramic://k3y52l7qbv1frxjdr9qpn9ldvbxb0jg4eig7wtjkdu6gk84vyazw9j4txf4o6d2io',
   CryptoAccounts: 'ceramic://k3y52l7qbv1fry6z45y9s2w5npe0nyokbp0oiv1tdhvswhv07lb2v13zdz4i1bp4w',
-  Definition: 'ceramic://k3y52l7qbv1frxhudkas7hj2z9l45y3w1nhejmj7ykoyrlukqwd160233l7qupp8g',
+  Definition: 'ceramic://k3y52l7qbv1fry4h28r53ebkeym2mxf1i23f303ql3gs1oqovfml44scaukncpxc0',
   IdentityIndex: 'ceramic://k3y52l7qbv1frxrcmx299lbnc5txfo4b7tls1rm5vf7luc34yuztc60tibruptp8g',
   ThreeIdKeychain: 'ceramic://k3y52l7qbv1fry69sodu0hc4nwvzglaxxvy3l0xdw7v1o36gyu2dl9us472qh8veo'
 }
@@ -97,7 +96,7 @@ export type DefinitionType<K extends DefinitionName> = DefinitionTypes[K]
 export type PublishedDefinitions = PublishedRecord<DefinitionName>
 
 export const definitions: PublishedDefinitions = {
-  basicProfile: 'kjzl6cwe1jw14bdsytwychcd91fcc7xibfj8bc0r2h3w5wm8t6rt4dtlrotl1ou',
-  cryptoAccounts: 'kjzl6cwe1jw1490kh2uqjxafo4xy0oxtufuiscvplhoo8fqz3uonm1cwnfw0x5j',
-  threeIdKeychain: 'kjzl6cwe1jw148786wkzw43y8jdqw3maliueon9regm5zp8hxh4gpk2gjg4dtyz'
+  basicProfile: 'kjzl6cwe1jw14aewu2t91dwgc4ezkf74kmqe9beuxz8u1tlpjfss6nibbwbkohw',
+  cryptoAccounts: 'kjzl6cwe1jw14bek5i7rcr1q9byw61w4rswrhmvja0kfos89ty0notx0vh7kx3b',
+  threeIdKeychain: 'kjzl6cwe1jw14742krx8kpze2a2t25hwic331sljucncxt8g3ld8duj6cs0w7to'
 }
